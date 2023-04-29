12be9d7f0c346fa8cfcbbf508ad84659b3ea074e
Danny Bui <Dannybui5155@gmail.com>
2023-04-29 13:37:58 -0500

    created a static website that will serve as a starting point for the web-based todo app

---
+++ web.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("I made this app to improve your productivity.")
+
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Add new todo...")
